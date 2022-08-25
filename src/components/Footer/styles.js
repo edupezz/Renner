568016dad16b3b0cd4e19b39d21eaddb420cd477
Eduardo <eduardo.pezzi@fohat.co>
2022-08-25 00:04:25 -0300

    Add: main structure and global styles

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  height: 10vh;
+  background-color: honeydew;
+`;
---
+++ src/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    width: 100vw;
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+  html {
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyle;
